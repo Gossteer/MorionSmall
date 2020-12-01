d101e9ad2aaf8aa23d5ac172ec53d08c58590985
Gossteer <gossteerplus@gmail.com>
2020-12-01 16:42:24 +0300

    DIY template engine start

--- buildozer/Core/View/ShowView.php
+++ buildozer/Core/View/ShowView.php
@@ -8,6 +8,24 @@ class ShowView implements iView
 {
     public function openPages(string $pathviews)
     {
-        return require('./Views/' . $pathviews . '.html');
+        // $path = './Views/' . $pathviews . '.html';
+
+        // if (is_readable($path)) {
+        //     return require($path);
+        // } else {
+        //     throw new Exception('Невозможно найти/прочитать представление '.$path);
+        // }
+
+        $title = " <i>Тестовая страница</i>";
+        $meta = "тест, шаблонизатор, php";
+        $content = "Добро пожаловать на тестовую страницу шаблонизатора. У нас всё вышло! УРА!!!";
+        
+        // шаблонизатор
+        $file = file_get_contents('./Views/' . $pathviews . '.html'); // путь к шаблону
+        $file=str_replace('{title}',$title,$file); // обьявляем из каких переменых брать информацию
+        $file=str_replace('{meta}',$meta,$file);
+        $file=str_replace('{content}',$content,$file);
+        print $file; //выводим
+
     }
 }
--- index.php
+++ index.php
@@ -1,20 +1,41 @@
 <?php
 
-define("ROOT_DIR", __DIR__ .'/');
+'https://code.tutsplus.com/tutorials/roll-your-own-templating-system-in-php--net-16596';
+'https://github.com/twigphp/Twig';
+'https://github.com/janl/mustache.js/';
+'DIY php template engine';
+'https://medium.com/shecodeafrica/building-your-own-custom-php-framework-part-2-c4de74e948a5';
 
-require_once "buildozer/autoload.php"; //автозагрузчик классов
-require_once "buildozer/main.php"; //основной класс приложения
+// function load_envato_blog_posts( $site = 'themeforest' ) { 
+//     $url = 'http://marketplace.envato.com/api/edge/blog-posts:' . $site . 'json'; 
+//     $entries = array(); 
+//     $ch = curl_init(); 
+//     curl_setopt($ch, CURLOPT_URL, $url); 
+//     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5); 
+//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
+//     $ch_data = curl_exec($ch); 
+//     curl_close($ch); 
+    
+//     if(!empty($ch_data)) { 
+//     $json_data = json_decode($ch_data, TRUE); 
+//     foreach( $json_data['blog-posts'] as $entry ) { 
+//         $entries[] = (object) $entry; 
+//     } 
+//     return $entries; 
+//     } 
+//     else { 
+//         die('Something went wrong with the API request!'); 
+//     } 
+// }
 
-$application = new Application();    
-$application->run();
+// define("ROOT_DIR", __DIR__ .'/');
 
-$firstview = new ShowView();
-$firstview->openPages('first');
+// require_once "buildozer/autoload.php"; //автозагрузчик классов
+// require_once "buildozer/main.php"; //основной класс приложения
 
-// use Core\View\ShowView;
+// $application = new Application();    
+// $application->run();
 
-// $lol = new ShowView();
+// $firstview = new ShowView();
+// $firstview->openPages('first');
 
-// $lol->openPages("first");
-
-// echo('lol');
